ebbbabae7754e4405db1899ca7cc8b1b8f45fa79
Trillion_Impulse <tkdduq2897@naver.com>
2025-04-30 21:35:12 +0900

    test(routing/server.py): 라우팅을 통한 출력 기능 테스트
    - 중복된 HTML 구조를 template() 함수로 분리
    - 목록 생성을 getContents() 함수로 분리
    - 라우트 경로 /read/<int:id>/를 사용해 동적 출력 기능 구현

--- server.py
+++ server.py
@@ -9,31 +9,44 @@ topics = [
     {'id': 3, 'title': 'javascript', 'body': 'javascript is...'}
 ]
 
-@app.route('/')
-def index():
-    liTags =''
-    for topic in topics:
-        liTags = liTags + f'<li><a href={topic["id"]}>{topic["title"]}</a></li>'
+def template(contents, content):
     return f'''<!doctype html>
     <html>
         <body>
             <h1><a href="/">WEB</a></h1>
             <ol>
-                {liTags}
+                {contents}
             </ol>
-            <h2>Welcome</h2>
-            Hello, Web
+            {content}
         </body>
     </html>
     '''
+
+def getContents():
+    liTags =''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return liTags
+
+@app.route('/')
+def index():
+    return template(getContents(), '<h2>Welcome</h2>Hello, Web')
 
 @app.route('/Create/')
 def Create():
     return 'Create'
 
-@app.route('/read/<id>/')
+@app.route('/read/<int:id>/')
 def read(id):
-    return 'ID: '+id
+    title = ''
+    body = ''
+    for topic in topics:
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+
+    return template(getContents(),f'<h2>{title}</h2>{body}')
 
 
 app.run(debug=True)
