23ea8a1433c5e85d9da6af234384b38c2361026a
Trillion_Impulse <tkdduq2897@naver.com>
2025-07-11 22:12:34 +0900

    feat(auth/auth.py): 회원가입(register) 기능 뷰 함수 추가

--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -13,3 +13,32 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 # 애플리케이션 객체처럼, 블루프린트도 어디에 정의되었는지 알아야 하므로 __name__이 두 번째 인자로 전달
 # url_prefix='/auth'는 이 블루프린트에 정의된 모든 URL 앞에 /auth가 자동으로 추가
 # 예: @bp.route('/login') → 실제 경로는 /auth/login
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
